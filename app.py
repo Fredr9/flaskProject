ff73ea8cf9369ed3c24612c13fd5be0024be5605
fredrik <fredrik_mv_90@hotmail.com>
2021-04-08 10:37:33 +0200

    Implemented a simple adduser, curl 127.0.0.1:5000/users ["{\n    \"id\": \"5b7650ea-9845-11eb-99fd-671839f5fdcb\",\n    \"username\": \"Finn Marius\"\n}","{\n    \"id\": \"62494d3c-9845-11eb-99fd-671839f5fdcb\",\n    \"username\": \"Finn\"\n}"] Works something like this

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import uuid
 
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 # State when starting
 users = []
 
+
 @app.route('/chat-rooms')
 def chatRooms():
     return 'Hallo Joakim!'
 
+
 # Gets all the users in the system.
 @app.route('/users', methods=['GET'])
 def getUsers():
-    return jsonify([user for user in users])
+    return jsonify([user.toJSON() for user in users])
+
 
 # ADd new user
 @app.route('/users', methods=['POST'])
@@ -25,11 +29,11 @@ def addUsers():
     id = uuid.uuid1()
     user = User(id, username)
     users.append(user)
-    return jsonify([user for user in users])
+    return jsonify(user.id)
 
 
 # Find one specific user
-@app.route('/messages')
+@app.route('/users/<userId>', methods=['GET'])
 def getUser(userId):
     for i in range(len(users)):
         if users[i].id == userId:
@@ -37,13 +41,40 @@ def getUser(userId):
 
     return "No such user"
 
+
+# Delete one specific user
+@app.route('/users/<userId>', methods=['DELETE'])
+def delete(userId):
+    index = findSpecificIndex(userId)
+    if index == -1:
+        return "Cant find user"
+
+    del users[index]
+    return "User deletet"
+
+
+# Function that find index of user
+def findSpecificIndex(userId):
+    for i in range(len(users)):
+        if users[i].id == userId:
+            return i
+    return -1  # Did not find user
+
+
 @app.route('/messages')
 def getMessages():
     return 'Hello, this is a message'
+
 
 @app.route('/')
 def server_start():
     return 'You need to specify path'
 
 
+class User:
+    def __init__(self, id, username):
+        self.id = str(id)
+        self.username = username
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
