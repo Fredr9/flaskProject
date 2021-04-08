d55331e8e30e9d1acd2fab657fa603164d8f03fb
fredrik <fredrik_mv_90@hotmail.com>
2021-04-08 09:21:27 +0200

    Added addusers, havent gotten it fully functional yet.

--- app.py
+++ app.py
@@ -1,30 +1,49 @@
+import uuid
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# State when starting
 users = []
 
 @app.route('/chat-rooms')
 def chatRooms():
     return 'Hallo Joakim!'
 
+# Gets all the users in the system.
 @app.route('/users', methods=['GET'])
 def getUsers():
     return jsonify([user for user in users])
 
+# ADd new user
 @app.route('/users', methods=['POST'])
 def addUsers():
-    content = request.data
-
+    content = request.json
+    username = content['username']
+    # Random ID to users
+    id = uuid.uuid1()
+    user = User(id, username)
+    users.append(user)
     return jsonify([user for user in users])
+
+
+# Find one specific user
+@app.route('/messages')
+def getUser(userId):
+    for i in range(len(users)):
+        if users[i].id == userId:
+            return users[i].toJson()
+
+    return "No such user"
 
 @app.route('/messages')
 def getMessages():
-    return 'Hallo Joakim! Det var dumt ja!'
+    return 'Hello, this is a message'
 
 @app.route('/')
-def hello_world():
-    return 'Hallo Joakim! has'
+def server_start():
+    return 'You need to specify path'
 
 
 
