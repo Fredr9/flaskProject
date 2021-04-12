707dfdbe34b264e11b6caa09304fe7d7538d39d3
fredrik <fredrik_mv_90@hotmail.com>
2021-04-12 16:21:16 +0200

    Changed some code to get POST and GET chat-rooms to work

--- app.py
+++ app.py
@@ -7,32 +7,20 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-
 # State when starting
 users = []
 chatrooms = []
 
-# Define chatRooms
-@app.route('/chat-rooms')
-def chatRooms():
-    return 'Here should the chat-rooms appear, E.G 127.0.0.1:5000/chat-rooms'
 
-# ADd new chatroom
-@app.route('/chat-room', methods=['POST'])
-def addChatroom():
-    content = request.json
-    chatroomname = content['chat-room']
-    # Random ID to users
-    idchatroom = uuid.uuid1()
-    chatroom = Chatroom(idchatroom, chatroomname)
-    users.append(chatroom)
-    return jsonify(chatroom.id)
+class Chatroom:
+    def __init__(self, id, roomname):
+        self.id = str(id)
+        self.roomname = roomname
 
-#Abort functions
 
-#reqparser() use
+    def toJSONChatroom(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-# define the classes chatrooms, messages and users
 
 class User:
     def __init__(self, id, username):
@@ -43,21 +31,57 @@ class User:
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
+# Define chatRooms
+#
 
-class Chatroom:
-    def __init__(self, roomname, roomid):
-        self.roomname = str(roomname)
-        self.roomid = roomid
+@app.route('/chat-rooms')
+def chatRooms():
+    return 'Here should the chat-rooms appear, E.G 127.0.0.1:5000/chat-rooms'
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+@app.route('/chat-room', methods=['GET'])
+def getChatrooms():
+    return jsonify([chatroom.toJSONChatroom() for chatroom in chatrooms])
 
+'''
+@app.route('/chat-room', methods=['GET'])
+def getChatrooms():
+    return jsonify([chatroom.toJSONChatroom()] for chatroom in chatrooms)
+'''
+
+
+# ADd new chatroom
+@app.route('/chat-room', methods=['POST'])
+def addChatrooms():
+    content = request.json
+    chatroomname = content['chat-room']
+    # Random ID to chatrooms
+    idchatroom = uuid.uuid1()
+    chatroom = Chatroom(idchatroom, chatroomname)
+    chatrooms.append(chatroom)
+    return jsonify(chatroom.id)
+
+
+@app.route('/chat-room/<chatroomID>', methods=['GET'])
+def getChatroom(chatroomID):
+    for i in range(len(chatrooms)):
+        if chatrooms[i].chatroomID == chatroomID:
+            return chatrooms[i].toJson()
+
+    return "No such chatroom"
+
+
+# Abort functions
+
+# reqparser() use
+
+# define the classes chatrooms, messages and users
 
 
 # functions to do a specific task
 # command Api.add_resource(name of the class, route)
 # after run app.run(debug, deport)
 # Gets all the users in the system.
+
 @app.route('/users', methods=['GET'])
 def getUsers():
     return jsonify([user.toJSON() for user in users])
@@ -113,6 +137,4 @@ def getMessages():
 def server_start():
     return 'You need to specify path'
 
-
-
-#class Messages:
+# class Messages:
