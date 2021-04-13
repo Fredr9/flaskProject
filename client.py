d8c021b8871a9a00ec64298cbb1a50de1fe41d7d
fredrik <fredrik_mv_90@hotmail.com>
2021-04-13 20:58:33 +0200

    Added some of the operations to the client

--- client.py
+++ client.py
@@ -8,21 +8,34 @@ from flask_restful import Api, Resource, reqparse, abort
 
 
 url = "http://localhost:5000/api"
-hello = input()
-Hello =sys.argv[0]
+chooser = input().lower()
+#Hello = sys.argv[0]
 
 
 #showUser = requests.get('http://127.0.0.1:5000/api/chat-rooms')
 #print(showUser)
-if hello == "adduser":
+if chooser == "adduser":
     username = {'username': input()}
     newUser = requests.post('http://127.0.0.1:5000/api/users', json=username)
     print(newUser.json())
 
+if chooser == "getusers":
+    getUser = requests.get('http://127.0.0.1:5000/api/users')
+    print(getUser.json())
+
+if chooser == "getchatrooms":
+    getChatroom = requests.get('http://127.0.0.1:5000/api/chat-rooms')
+    print(getChatroom.json())
+
+if chooser == "addchatrooms":
+    chatroom = {'chat-room': input()}
+    newChatroom = requests.post('http://127.0.0.1:5000/api/chat-rooms', json=chatroom)
+    print(newChatroom.json())
 
 
-#getChatroom = requests.get('http://127.0.0.1:5000/api/chat-rooms')
-#print(getChatroom.json())
+if chooser not in ("adduser", "getusers", "getchatrooms", "addchatrooms"):
+    print("You need to specify what you want to do!")
+
 #r = requests.get('http://127.0.0.1:5000/api/users')
 #print(newUser.json())
 
