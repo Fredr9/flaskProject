7ecf1ed4927a5a3d2f19401a94ea78a7cc5b8e8a
fredrik <fredrik_mv_90@hotmail.com>
2021-04-18 13:22:50 +0200

    Added bot functionality so bot can add chatroom if there isnt any.

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import requests
 
 url = "http://localhost:5000/api"
 # chooser = input().lower()
-botname = "Joakim"
+botname = sys.argv[1]
 
-#list of words
+# list of words
 
 greetings = ["Hello", "Hi", "Hello there", "Heihei"]
 
@@ -32,26 +32,39 @@ if botname == "Joakim":
         # Make a variable with room id extracted
         roomId = room['id']
         # Bot joining the first room:
+        f = requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/users'.format(roomId), json={"userId": userid},
+                          headers={"Content-Type": "application/json"})
+        print(f.status_code)
+        '''
         requests.post('/api/chat-rooms/<chatroomID>/users'.format(roomId), json={"userId": userid},
                       headers={"Content-Type": "application/json"})
-
+        '''
         # Bot sends message:
         random.choice(greetings)
-        #text = "testing"
-        '''
-        requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/{}/messages'.format(roomId, userid),
+        text = "testing"
+
+        re = requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/{}/messages'.format(roomId, userid),
                       data=text.format(text),
                       json={"userId": userid},
                       headers={"Content-Type": "application/json"})
+        print("HER DA?", re.status_code)
+        '''       
+        q = requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/{}/messages'.format(roomId, userid), json={"text": texst},
+                      headers={"Content-Type": "application/json"})
+        print(q)
+        #print(f'A bot has joined, {roomId}'.format(roomId))
                       '''
     else:
-        print("No room exists")
+        print("No room exists, you need to create one")
+        # Create a room:
+        chatroom = {'chat-room': input()}
+        newChatroom = requests.post('http://127.0.0.1:5000/api/chat-rooms', json=chatroom)
+        print(newChatroom.json())
         print(data)
 else:
     print("ukjent bot")
 
-
-botname = "Fredrik"
+# botname = "Fredrik"
 # Just a duplicate for now
 if botname == "Fredrik":
     username = {'username': 'Fredrik'}
@@ -75,20 +88,29 @@ if botname == "Fredrik":
         requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/users'.format(roomId), json={"userId": userid},
                       headers={"Content-Type": "application/json"})
         # Bot sends message:
-
-
-        requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/messages'.format(roomId), json={"text": "test 123"},
+        requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/messages'.format(roomId), json={"userId": userid},
                       headers={"Content-Type": "application/json"})
-        print(requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/messages'.format(roomId), json={"text": "test 123"},
-                      headers={"Content-Type": "application/json"}))
 
+        '''
+        requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/messages'.format(roomId),
+                      json={"text": "test 123"},
+                      headers={"Content-Type": "application/json"})
+        print(requests.post('http://127.0.0.1:5000/api/chat-rooms/{}/messages'.format(roomId),
+                            json={"text": "test 123"},
+                            headers={"Content-Type": "application/json"}))
+
+
+'''
 
     else:
         print("There isnt a room to join")
+        print(data)
+        print("Dette er en bot")
 
 else:
     print("ukjent bot")
 
-
+'''
 if __name__ == "__main__":
     print('test 123')
+'''
